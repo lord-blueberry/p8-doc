1178ed6876cc7a615ae21c42b478ded0326fdd5e
Jonas Schwammberger <jonas.schwammberger@fhnw.ch>
2019-01-17 19:17:12 +0100

    found better formulation for results

--- eval-p8/complexity.r
+++ eval-p8/complexity.r
@@ -1,13 +1,13 @@
-N = (8192*4) * (8192*4)
+N = (8192) * (8192)
 M = 540 * 4060770
 
 W = 128
 I.major = 6
 I.clean = 35000
 
-S = 500
+S = 3000
 I.cd = 1
-J=10
+J=5
 
 
 ld.N = log(N, base=2)
@@ -16,10 +16,10 @@ clean = I.major * 2 * (M + W*(2*N*ld.2N+2*N) + N * ld.N)
 clean = clean + I.major * (I.clean * 2 * N)
 
 CD <- function(S,M, I.cd, J) {
-  return(cd = S * 3 * M + I.cd * (S * 8*M + J*2*M))
+  return(cd = S * 7 * M + I.cd * (S * 4*M + J*2*M))
 }
 
-cd_cycles <- c(2, 4, 8)
+cd_cycles <- c(1, 4, 8)
 results <- c()
 for (i in cd_cycles) {
   res <-clean/CD(S, M, i, J)
